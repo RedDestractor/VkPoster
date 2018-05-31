3efec49c65921d9e2afa12f39537c1068c54fac8
Michael H <hamhoev-m@mail.ru>
2018-05-31 12:41:50 +0700

    continued starting :)

---
+++ VkPoster/Service/AuthentificationService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using VkPoster.Model;
+
+namespace VkPoster.Service
+{
+    public class AuthentificationService
+    {
+        public const string AppId = "6495092";
+
+        public UserInfo GetUserInfo()
+        {
+
+            //  well, Im gonna sleep
+
+        }
+
+    }
+}
